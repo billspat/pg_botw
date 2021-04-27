f8d49f0346ccc58a1c3394b156278e058cd99614
Patrick Bills <billspat@msu.edu>
2021-04-27 10:32:13 -0400

    example of Renviron file for db connection params

--- exampleRenviron.sh
+++
@@ -1,8 +0,0 @@
-# example .Renviron file for using Azure postgres.   
-# Replace <placeholders> with actual values and save as filename .Renviron
-# and restart your R session
-# use single quotes around DBUSER and DBPASSWORD values. 
-DBHOST=<yourservername>.postgres.database.azure.com
-DBPORT=5432
-DBUSER='<yourdbadminid>n@<yourservername>'
-DBPASSWORD='<yourpassword>'
